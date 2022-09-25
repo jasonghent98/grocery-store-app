349269cfb97738c3e08c8107a7ea303b159f17ff
Jason Ghent <jasonghent1008@gmail.com>
2022-09-24 17:19:29 -0700

    about us

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,9 +5,9 @@ const Navbar = () => {
   return (
     <div className='flex flex-row-reverse w-screen h-full bg-[#00703D]'>
             <Link
-            className='flex flex-col transition hover:bg-[#FF6B18] hover:text-[#FF6B18] delay-150'
+            className='flex flex-col transition hover:bg-[#FF6B18] hover:text-[#FF6B18 delay-150'
             href={'/about'}>
-                <div className="flex justify-center items-center relative top-14 right-4 bg-gray-200 h-2/6 w-1/6 rounded-sm transition text-[#FF6B18] hover:bg-[#FF6B18] hover:text-gray-200 bold delay-150">
+                <div className="flex justify-center items-center relative top-14 right-4 bg-gray-200 h-2/6 w-1/6 rounded-sm transition text-[#FF6B18] hover:cursor-pointer hover:bg-[#FF6B18] hover:text-gray-200 bold delay-150">
                     About Us
                 </div>
             </Link>
