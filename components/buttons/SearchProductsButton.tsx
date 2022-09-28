af802ea5867cb819912efd204e0d7360d46a7098
Jason Ghent <jasonghent1008@gmail.com>
2022-09-28 12:35:00 -0700

    item handler function + backend receiving data.

--- components/buttons/SearchProductsButton.tsx
+++ components/buttons/SearchProductsButton.tsx
@@ -1,13 +1,19 @@
+import axios from 'axios'
 import React from 'react'
 
 const SearchProductsButton = ({item}: any) => {
   
-  const sendItemToBackend = () => {
-    console.log(item)
+  const itemHandler = async () => {
+    if (item) {
+      const data = {
+        item
+      }
+      await axios.post('http://localhost:3000/api/getProducts', data)
+    }
   }
 
   return (
-    <div data-testid="searchProductsButton" className='flex justify-center items-center bg-[#FF6B18] rounded-md h-1/3 hover:drop-shadow-lg' onClick={sendItemToBackend}>
+    <div data-testid="searchProductsButton" className='flex justify-center items-center bg-[#FF6B18] rounded-md h-1/3 hover:drop-shadow-lg' onClick={itemHandler}>
         <div className='flex justify-center items-center px-4'>
             <div className='xl:text-2xl'>Search Item Nearby</div>
         </div>
