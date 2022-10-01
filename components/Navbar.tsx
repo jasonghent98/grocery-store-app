4100aaf41fc5ab834d6013c81af709795fd39e72
Jason Ghent <jasonghent1008@gmail.com>
2022-10-01 00:10:55 -0700

    conditional routing with dynamic routing button implemented

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Link from 'next/link'
 import RouteButton from './buttons/routeButton'
+import {useRouter} from 'next/router'
 
 const Navbar = () => {
+  const router = useRouter()
   return (
     <div className='flex flex-row-reverse w-screen h-full bg-[#00703D]'>
       <div>
-        <RouteButton text='About us' route='/about'/>
+        {/* render the about button if we're on the home page, and the searchItem button if we're on the about page*/}
+        {router.pathname === '/' && <RouteButton text='About us' route='/about'/>}
+        {router.pathname === '/about' && <RouteButton text='Search Item' route='/'/>}
+        
       </div>
     </div>
   )
