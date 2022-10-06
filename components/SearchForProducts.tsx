368159bbbb3bc0f0fd9c7c8ded99185193f45e20
Jason Ghent <jasonghent1008@gmail.com>
2022-10-06 17:48:06 -0400

    button styling and mobile friendly styling for search page

--- components/SearchForProducts.tsx
+++ components/SearchForProducts.tsx
@@ -10,7 +10,7 @@ const SearchForProducts = () => {
         <SearchBarOverlay>
             <div className='flex flex-col justify-between h-5/6 w-3/5'>
               <div className='flex flex-row justify-center items-center h-1/3'>
-                <p className='lg:text-3xl md:text-3xl text-xl'>What are you searching for?</p>
+                <p className='xl:text-6xl lg:text-5xl md:text-4xl text-2xl text-center '>What are you searching for?</p>
               </div>
               <div className='w-full h-1/3'>
                 <input 
--- components/buttons/SearchProductsButton.tsx
+++ components/buttons/SearchProductsButton.tsx
@@ -16,9 +16,9 @@ const SearchProductsButton = ({item}: any) => {
   }
 
   return (
-    <div data-testid="searchProductsButton" className='flex justify-center items-center bg-[#FF6B18] rounded-md h-1/3 hover:drop-shadow-lg' onClick={itemHandler}>
+    <div data-testid="searchProductsButton" className='flex justify-center items-center bg-[#FF6B18] rounded-md h-1/3 hover:drop-shadow-lg transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-105 duration-300' onClick={itemHandler}>
         <div className='flex justify-center items-center px-4'>
-            <div className='xl:text-2xl'>Search Item Nearby</div>
+            <div className='sm:text-lg md:text-xl lg:text-2xl xl:text-3xl text-center font-semibold'>Search Item Nearby</div>
         </div>
     </div>
   )
