d25dc331a29c4f39d40068ee8591d21c6adf990b
Jason Ghent <jasonghent1008@gmail.com>
2022-10-12 16:17:58 -0400

    dynamic route created for individual results

---
+++ pages/results/[productId].tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+
+// dynamic page that will contain all the detailed information about a searched product in the client's area
+// need to ensure the api results contain locations so that we can provide a map for the user for locating stores
+
+const Product = () => {
+    const router = useRouter()
+    const {productId} = router.query
+  return (
+    <div>{productId}</div>
+  )
+}
+
+export default Product
