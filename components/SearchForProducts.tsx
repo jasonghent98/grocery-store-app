506524d01dd803634d52a822430949a7048d99cd
Jason Ghent <jasonghent1008@gmail.com>
2022-12-18 20:17:41 -0500

    scripts for production and dev envs with docker

--- components/SearchForProducts.tsx
+++ components/SearchForProducts.tsx
@@ -9,7 +9,7 @@ const SearchForProducts = () => {
         <SearchBarOverlay>
             <div className='flex flex-col justify-between h-5/6 w-3/5'>
               <div className='flex flex-row justify-center items-center h-1/3'>
-                <p className='xl:text-6xl lg:text-5xl md:text-4xl text-2xl text-center '>What are you searching for? dude...</p>
+                <p className='xl:text-6xl lg:text-5xl md:text-4xl text-2xl text-center '>What are you searching for?</p>
               </div>
               
               <SearchBar styles={'rounded-lg w-full h-2/5 text-black bg-gray-200 relative placeholder:italic pl-3 placeholder:text-lg placeholder:sm:text-xl placeholder:md:text-2xl placeholder:lg:text-3xl placeholder:xl:text-4xl'} />
